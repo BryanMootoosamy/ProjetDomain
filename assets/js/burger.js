a7781bfce6e16d9b129aa228a3a8cf5530dfd7a5
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-04-11 20:51:48 +0200

    maj responsive

---
+++ assets/js/burger.js
@@ -0,0 +1,3 @@
+let menu = document.getElementById("bottom");
+let burgerButton = document.getElementById("burgerButton");
+burgerButton.addEventListener("click", function() {menu.classList.toggle("show");});
--- view/head.php
+++ view/head.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="assets/style/style.css">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
         <meta name="viewport" content="width=device-width">
         <?php
             if ($currentPage == "portfolio") {
