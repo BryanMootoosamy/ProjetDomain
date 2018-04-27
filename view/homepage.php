d0e6e4b5825826b6c52e22cad08b9af4a45d38bc
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-04-27 19:32:23 +0200

    maj style & bugfix

--- view/homepage.php
+++ view/homepage.php
@@ -1,6 +1,17 @@
 
     
-        <section class="main">
-            <h2>SITE EN CONSTRUCTION</h2>
-            <h4>Sauvez les arbres, mangez des castors</h4>
-        </section>
+    <div class="sp-container">
+        <div class="sp-content">
+            <div class="sp-globe"></div>
+            <h2 class="frame-1">BIENVENUE</h2>
+            <h2 class="frame-2">PRENEZ UN SIÈGE</h2>
+            <h2 class="frame-3">PENSEZ À VISITER MON PORTEFOLIO</h2>
+            <h2 class="frame-4">C'EST SYMPA</h2>
+            <h2 class="frame-5">
+                <span class="span1">PASSIONNÉ,</span>
+                <span class="span2">TRAVAILLEUR,</span>
+                <span class="span3">MOTIVÉ.</span>
+            </h2>
+            <a class="sp-circle-link" href="https://github.com/BryanMootoosamy?tab=repositories">Bryan</a>
+        </div>
+    </div>
