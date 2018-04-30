4d7aa7de4332e7a05dba11cc24307c63b08e74fd
BryanMootoosamy <mootoosamy.bryan@gmail.com>
2018-04-30 10:22:37 +0200

    Graphic and responsive update

--- view/header.php
+++ view/header.php
@@ -4,15 +4,15 @@
                 <h1>Bryan's Domain</h1>
                 <h5>"Oh look! A penny !"</h5>
             </div>
-            <div class="navMenu" id="navMenu">
-                <ul class="menu">
-                    <li><a class="menuButton" href="index.php">Homepage</a></li>
-                    <li id="flux"><a class="menuButton" href="index.php?action=espaceperso">Espace Personnel</a></li>
-                    <li><a class="menuButton" href="index.php?action=portfolio">Portfolio</a></li>
-                </ul>
-            </div>
         </section>
     </header>
+    <div class="navMenu" id="navMenu">
+        <ul class="menu">
+            <li><a class="menuButton" href="index.php">Homepage</a></li>
+            <li id="flux"><a class="menuButton" href="index.php?action=espaceperso">Espace Personnel</a></li>
+            <li><a class="menuButton" href="index.php?action=portfolio">Portfolio</a></li>
+        </ul>
+    </div>
     <div id="burgerButton">
         <div id="lign1"></div>
         <div id="lign2"></div>
--- view/portfolio.php
+++ view/portfolio.php
@@ -1,25 +1,25 @@
 
-        <main id="folio">
-            <h2>A propos</h2>
-            <div id="imageHead">
-                <!-- <img src="assets/pics/face.jpg" alt="Photo de profil"> -->
-            </div>
-            <section class="content">
-                <section class="left">
-                    <h3>Petite Bio</h3>
-                    <p>Ancien étudiant en Droit à l'Université de Mons, j'ai changé d'orientation afin de me focaliser sur mes passion,
-                    l'informatique et le développement. J'ai donc intégré la formation BeCode (plus particulièrement la première promotion du centre de formation de
-                    Charleroi situé au Quai 10). Mon objectif est d'être Full Stack / polyvalent afin de pouvoir répondre à n'importe quel besoin.</p>
-                </section>
-                <section class="right">
-                    <h3>Portfolio</h3>
-                    <ul>
-                        <li>PHP + mySQL: <a href="https://mootoosamybryan.be/php-chat-db/index.php">Tchat en ligne</a></li>
-                        <li>Design + premier MVC: <a href="https://mootoosamybryan.be/MVC%20Demotte/">Reproduction du site de Didier Motte</a></li>
-                        <li>Apprentissage du CMS Wordpress: <a href="https://mootoosamybryan.be/foodog/">FooDog</a></li>
-                        <li>Jeu Clicker (toujours en développement): <a href="https://mootoosamybryan.be/sausageclicker/">Sausage Clicker</a></li>
-                        <li>Jeu utilisant Phaser (toujours en développement): <a href="https://mootoosamybryan.be/uppercards/">UpperCards</a></li>
-                    </ul>
-                </section>
+    <main id="folio">
+        <h2>A propos</h2>
+        <div id="imageHead">
+            <!-- <img src="assets/pics/face.jpg" alt="Photo de profil"> -->
+        </div>
+        <section class="content">
+            <section class="left">
+                <h3>Petite Bio</h3>
+                <p>Ancien étudiant en Droit à l'Université de Mons, j'ai changé d'orientation afin de me focaliser sur mes passion:
+                l'informatique et le développement. J'ai donc intégré la formation BeCode (plus particulièrement la première promotion du centre de formation de
+                Charleroi situé au Quai 10). Mon objectif est d'être Full Stack / polyvalent afin de pouvoir répondre à n'importe quel besoin.</p>
             </section>
-        </main>
+            <section class="right">
+                <h3>Portfolio</h3>
+                <ul>
+                    <li>PHP + mySQL: <a href="https://mootoosamybryan.be/php-chat-db/index.php">Tchat en ligne</a></li>
+                    <li>Design + premier MVC: <a href="https://mootoosamybryan.be/MVC%20Demotte/">Reproduction du site de Didier Motte</a></li>
+                    <li>Apprentissage du CMS Wordpress: <a href="https://mootoosamybryan.be/foodog/">FooDog</a></li>
+                    <li>Jeu Clicker (toujours en développement): <a href="https://mootoosamybryan.be/sausageclicker/">Sausage Clicker</a></li>
+                    <li>Jeu utilisant Phaser (toujours en développement): <a href="https://mootoosamybryan.be/uppercards/">UpperCards</a></li>
+                </ul>
+            </section>
+        </section>
+    </main>
